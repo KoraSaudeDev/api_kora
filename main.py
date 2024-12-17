cb17131c7a25d2cbe9b7bb2f87c911f186de4ec8
Hugo-kora <hugo.silva@korasaude.com.br>
2024-12-17 16:14:43 -0300

    subindo aceitando cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flasgger import Swagger
+from flask_cors import CORS  # Importação do CORS
 from app.controllers.auth_controller import auth_bp
 from app.controllers.verzo_controller import verzo_bp
 from app.controllers.route_controller import route_bp
@@ -8,6 +9,9 @@ from app.controllers.system_controller import system_bp
 from app.controllers.user_controller import user_bp
 
 app = Flask(__name__)
+
+# Configurando CORS para aceitar qualquer origem temporariamente
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Configuração do Swagger
 template = {
@@ -60,4 +64,4 @@ def home():
     return {"message": "Bem-vindo à API Verzo!"}, 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3793, debug=True)
+    app.run(host="0.0.0.0", port=3792, debug=True)
